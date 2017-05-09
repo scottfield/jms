5b4bc68e021ffcf4fa5379c13a22bf0a087c4b00
scottfield <scottfield@qq.com>
2017-05-09 10:35:36 +0800

    格式化代码

--- src/main/java/com/jackie/jms/Chat.java
+++ src/main/java/com/jackie/jms/Chat.java
@@ -29,14 +29,14 @@ public class Chat implements MessageListener {
     private TopicPublisher publisher;
     private TopicConnection connection;
     private String username;
+
     /* Constructor used to Initialize Chat */
     public Chat(String topicFactory, String topicName, String username)
             throws Exception {
 // Obtain a JNDI connection using the jndi.properties file
         InitialContext ctx = new InitialContext();
 // Look up a JMS connection factory and create the connection
-        TopicConnectionFactory conFactory =
-                (TopicConnectionFactory)ctx.lookup(topicFactory);
+        TopicConnectionFactory conFactory = (TopicConnectionFactory) ctx.lookup(topicFactory);
         TopicConnection connection = conFactory.createTopicConnection();
 // Create two JMS session objects
         TopicSession pubSession = connection.createTopicSession(
@@ -44,7 +44,7 @@ public class Chat implements MessageListener {
         TopicSession subSession = connection.createTopicSession(
                 false, Session.AUTO_ACKNOWLEDGE);
 // Look up a JMS topic
-        Topic chatTopic = (Topic)ctx.lookup(topicName);
+        Topic chatTopic = (Topic) ctx.lookup(topicName);
 // Create a JMS publisher and subscriber. The additional parameters
 // on the createSubscriber are a message selector (null) and a true
 // value for the noLocal flag indicating that messages produced from
@@ -63,42 +63,50 @@ public class Chat implements MessageListener {
 // Start the JMS connection; allows messages to be delivered
         connection.start();
     }
-    /* Receive Messages From Topic Subscriber */
-    public void onMessage(Message message) {
-        try {
-            TextMessage textMessage = (TextMessage) message;
-            System.out.println(textMessage.getText());
-        } catch (JMSException jmse){ jmse.printStackTrace(); }
-    }
-    /* Create and Send Message Using Publisher */
-    protected void writeMessage(String text) throws JMSException {
-        TextMessage message = pubSession.createTextMessage();
-        message.setText(username+": "+text);
-        publisher.publish(message);
-    }
-    /* Close the JMS Connection */
-    public void close() throws JMSException {
-        connection.close();
-    }
+
     /* Run the Chat Client */
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         try {
-            if (args.length!=3)
+            if (args.length != 3)
                 System.out.println("Factory, Topic, or username missing");
 // args[0]=topicFactory; args[1]=topicName; args[2]=username
-            Chat chat = new Chat(args[0],args[1],args[2]);
+            Chat chat = new Chat(args[0], args[1], args[2]);
 // Read from command line
             BufferedReader commandLine = new
                     java.io.BufferedReader(new InputStreamReader(System.in));
 // Loop until the word "exit" is typed
-            while(true) {
+            while (true) {
                 String s = commandLine.readLine();
-                if (s.equalsIgnoreCase("exit")){
+                if (s.equalsIgnoreCase("exit")) {
                     chat.close();
                     System.exit(0);
                 } else
                     chat.writeMessage(s);
             }
-        } catch (Exception e) { e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Receive Messages From Topic Subscriber */
+    public void onMessage(Message message) {
+        try {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println(textMessage.getText());
+        } catch (JMSException jmse) {
+            jmse.printStackTrace();
+        }
+    }
+
+    /* Create and Send Message Using Publisher */
+    protected void writeMessage(String text) throws JMSException {
+        TextMessage message = pubSession.createTextMessage();
+        message.setText(username + ": " + text);
+        publisher.publish(message);
+    }
+
+    /* Close the JMS Connection */
+    public void close() throws JMSException {
+        connection.close();
     }
 }
